de1f6ed0e3e88c443d48d779a67d6d8ceed2ee58
Loic Messal <loic.messal@orange.fr>
2016-04-03 15:17:13 +0200

    try openshift

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ var server = http.createServer(function(req, res) {
     });
 });
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port );
+});
+
 // Chargement de socket.io
 var io = require('socket.io').listen(server);
 
@@ -58,9 +65,3 @@ io.sockets.on('connection', function (socket, pseudo) {
 
 
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", server_port " + port )
-});
