ef2ddf5ccf165fd90d3d6708b7d7fddc26216327
Loic Messal <loic.messal@orange.fr>
2016-04-03 15:45:47 +0200

    configuration heroku

--- app.js
+++ app.js
@@ -19,12 +19,8 @@ var server = http.createServer(function(req, res) {
     });
 });
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.PORT || 8080 ; 
 
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", server_port " + port );
-});
 
 // Chargement de socket.io
 var io = require('socket.io').listen(server);
@@ -65,3 +61,4 @@ io.sockets.on('connection', function (socket, pseudo) {
 
 
 
+server.listen(server_port);
