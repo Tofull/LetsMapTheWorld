9b20265df5247632dac1646282a5b4f5b8557336
Tofull <loic.messal@orange.fr>
2016-09-20 14:23:42 +0200

    Correctif server

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ var server = http.createServer(function(req, res) {
     fs.readFile('./index.html', 'utf-8', function(error, content) {
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(content);
-    }
+    });
 });
-    
+
 
 var server_port = process.env.PORT || 8080 ; 
 
